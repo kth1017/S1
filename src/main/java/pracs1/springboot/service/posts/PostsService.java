97ec93579b83ce76b354362961c1378266ac2410
kth <rnjsxo1017@gmail.com>
2022-03-20 15:47:47 +0900

    gitignore 추가

--- src/main/java/pracs1/springboot/service/posts/PostsService.java
+++ src/main/java/pracs1/springboot/service/posts/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pracs1.springboot.domain.posts.Posts;
 import pracs1.springboot.domain.posts.PostsRepository;
+import pracs1.springboot.web.dto.PostsListResponseDto;
 import pracs1.springboot.web.dto.PostsResponseDto;
 import pracs1.springboot.web.dto.PostsSaveRequestDto;
 import pracs1.springboot.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -22,16 +26,32 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
 
-    public PostsResponseDto findById (Long id) {
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("게시글 없음 id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/pracs1/springboot/web/IndexController.java
+++ src/main/java/pracs1/springboot/web/IndexController.java
@@ -1,12 +1,22 @@
 package pracs1.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pracs1.springboot.service.posts.PostsService;
+import pracs1.springboot.web.dto.PostsResponseDto;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index"; // starter 의존성으로 주소, 확장자 자동 추가
     }
 
@@ -14,4 +24,13 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
+    @GetMapping("posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
+    }
+
+
 }
