ac45395edc314ba47df045e5fb996b52dbf22430
kth <rnjsxo1017@gmail.com>
2022-04-26 11:11:32 +0900

    중간 배포본 포트 수정

--- src/test/java/pracs1/springboot/web/PostsApiControllerTest.java
+++ src/test/java/pracs1/springboot/web/PostsApiControllerTest.java
@@ -30,7 +30,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
 
     @LocalServerPort
